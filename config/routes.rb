fbdb89a1c2db8038daf696741546b68980720e73
Yury Cavalcante <yurypcfarias@gmail.com>
2023-10-06 14:48:41 -0300

    feat: added route to get self user account details

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post '/create_user_account', to: 'user_account#create'
   post '/sign_in',             to: 'user_account#sign_in'
+  get  '/me',                  to: 'user_account#show'
 end
