d918eced7a5a03f7cfc4e9e8b2e042a3303ebf98
Yury Cavalcante <yurypcfarias@gmail.com>
2023-10-08 19:52:46 -0300

    feat: added transaction route to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # UserAccount related routes
   post '/create_user_account', to: 'user_account#create'
   post '/sign_in',             to: 'user_account#sign_in'
   get  '/me',                  to: 'user_account#show'
+
+  # Transaction related routes
+  #post '/reverse_transaction', to:
+  post '/transaction',         to: 'transaction#create'
+  #get  '/transactions',        to:
 end
