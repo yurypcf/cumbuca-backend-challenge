ff552336accdfc41ad80936a01d7ed9a5681c23b
Yury Cavalcante <yurypcfarias@gmail.com>
2023-10-10 18:13:14 -0300

    fix: removing todo that will not be done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # TODO: NAMESPACE Routes
   # UserAccount related routes
   post '/create_user_account', to: 'user_account#create'
   post '/sign_in',             to: 'user_account#sign_in'
